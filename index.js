d1100fe67ec6186853f85a39249465e82596b5f7
JuanMolina2001 <juanmolinajm2001@gmail.com>
2024-04-05 06:03:18 -0300

    Update port number from 3000 to 80

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const parseInfo = require('./parseInfo');
 const app = express();
-const port = 3000;
+const port = 80;
 const  { Agent, setGlobalDispatcher } = require('undici')
 
 const agent = new Agent({
