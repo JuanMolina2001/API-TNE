7bd787a7a5890b51b67d7dd44e899d6df9b550ec
JuanMolina2001 <juanmolinajm2001@gmail.com>
2024-04-05 06:15:46 -0300

    Update port to use environment variable

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const parseInfo = require('./parseInfo');
 const app = express();
-const port = 80;
+const port = process.env.PORT || 3000;
 const  { Agent, setGlobalDispatcher } = require('undici')
 
 const agent = new Agent({
